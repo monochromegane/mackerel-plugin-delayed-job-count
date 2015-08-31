19ef0cde4452fc210220beb63e9bb5c819aabebc
monochromegane <dev.kuro.obi@gmail.com>
2015-08-31 17:43:16 +0900

    Initial commit.

---
+++ delayed-job-count.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	mp "github.com/mackerelio/go-mackerel-plugin"
+	"github.com/monochromegane/dsn"
+)
+
+var env string
+var dbconf string
+
+var graphdef map[string](mp.Graphs) = map[string](mp.Graphs){
+	"delayed_job": mp.Graphs{
+		Label: "Delayed Job Count",
+		Unit:  "integer",
+		Metrics: [](mp.Metrics){
+			mp.Metrics{Name: "count", Label: "Job Count"},
+			mp.Metrics{Name: "error", Label: "Error Job Count"},
+		},
+	},
+}
+
+type JobCountPlugin struct {
+	Tempfile string
+}
+
+func (j JobCountPlugin) FetchMetrics() (map[string]float64, error) {
+
+	name, dsn, err := dsn.FromRailsConfig(dbconf, env)
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := sql.Open(name, dsn)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	var count float64
+	if err := db.QueryRow("SELECT COUNT(id) FROM delayed_jobs;").Scan(&count); err != nil {
+		return nil, err
+	}
+
+	var errCount float64
+	if err := db.QueryRow("SELECT COUNT(id) FROM delayed_jobs WHERE failed_at IS NOT NULL;").Scan(&errCount); err != nil {
+		return nil, err
+	}
+
+	return map[string]float64{
+		"count": count,
+		"error": errCount,
+	}, nil
+}
+
+func (j JobCountPlugin) GraphDefinition() map[string](mp.Graphs) {
+	return graphdef
+}
+
+func init() {
+	flag.StringVar(&env, "env", "development", "environment")
+	flag.StringVar(&dbconf, "dbconf", "", "Rails dbconfig file")
+	flag.Parse()
+}
+
+func main() {
+	helper := mp.NewMackerelPlugin(JobCountPlugin{})
+
+	if os.Getenv("MACKEREL_AGENT_PLUGIN_META") != "" {
+		helper.OutputDefinitions()
+	} else {
+		helper.OutputValues()
+	}
+}
